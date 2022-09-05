d848914a0d847dde0824e18916c255846e31bff2
Humberto Monteiro <humbertoaraujox@icloud.com>
2022-09-05 10:17:04 -0300

    estilos

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
+function toggleClass(elemento, tag) {
+    const toggle = document.querySelector(`${elemento}`).classList
+    toggle.toggle(`${tag}`)
+}
+
+function removerClass(elemento, tag) {
+    const remover = document.querySelector(`${elemento}`).classList
+    remover.remove(`${tag}`)
+}
+
+function addClass(elemento, tag) {
+    const add = document.querySelector(`${elemento}`).classList
+    add.add(`${tag}`)
+}
+
 //ajax para navegar html
 
 const hfNav = document.querySelectorAll('[hf-nav]')
 hfNav.forEach(link => {
     const conteudoHtml = document.getElementById('home')
-    const navHtml = document.querySelector('#navHtml').classList
 
     link.onclick = function(a) {
         a.preventDefault()
 
-        navHtml.remove('displayNone')
-        navHtml.add('displayBlock')
-        
+        removerClass('#navHtml', 'displayNone')
+        addClass('#css', 'displayNone')
+
         fetch(link.getAttribute('hf-nav'))
             .then(resp => resp.text())
             .then(html => conteudoHtml.innerHTML = html)
     }
 })
 
+//conteudo css
+
 const hfCss = document.querySelectorAll('[hf-css]')
 hfCss.forEach(link => {
-    const conteudoHtml = document.querySelector('#home')
-    
+    const conteudoHtml = document.getElementById('home')
+
     link.onclick = function(a) {
         a.preventDefault()
 
+        toggleClass('#css', 'displayNone')
+        addClass('#navHtml', 'displayNone')
+    
         fetch(link.getAttribute('hf-css'))
             .then(resp => resp.text())
             .then(html => conteudoHtml.innerHTML = html)
     }
+    
 })
 
 //ajax para navegar git
@@ -49,7 +69,6 @@ hfGit.forEach(link => {
 })
 
 
-
 //animação scroll
 
 window.sr = ScrollReveal({ reset: true })
@@ -63,4 +82,20 @@ sr.reveal('.scroll', {
 sr.reveal('.scroll1', { 
     rotate: {x: 0, y: 50, z:0},
     duration: 1500
+})
+
+//efeito vanilllia tilt
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
+})
+
+VanillaTilt.init(document.querySelectorAll(".card2"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
 })
